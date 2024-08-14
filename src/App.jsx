9201eac27ced624c7ca7776c94ccbd26750afc8a
yubaraj1357 <dasyubaraj2020@gmail.com>
2024-08-14 13:06:11 +0530

    created first react app

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Hello There !!</h1>
+      <div className="card">
+    I am Yuvraj✌️
+      </div>
+      <p className="read-the-docs">
+       I am a software engineer at codeXincube!
+      </p>
+    </>
+  )
+}
+
+export default App
